f16f2a48599b1f3ed8a95de443a76c1183413831
Igareck99 <igor9.i@yandex.ru>
2024-10-28 18:12:11 +0300

    Initial Commit

---
+++ YandexCup/YandexCupApp.swift
@@ -0,0 +1,17 @@
+//
+//  YandexCupApp.swift
+//  YandexCup
+//
+//  Created by Игорь Коноваленко on 28.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct YandexCupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
